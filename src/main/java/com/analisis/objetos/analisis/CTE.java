0b9aaf1ee7bf67630be9d3a2269a1e293e1aab14
Carlos Soberanis <jcsru13@gmail.com>
2020-11-23 02:59:10 -0600

    ADD: Inicializando y estructura del proyecto

---
+++ src/main/java/com/analisis/objetos/analisis/CTE.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.analisis.objetos.analisis;
+
+/**
+ *
+ * @author jose_
+ */
+public enum CTE {
+    
+    p,h,ENTERO,CARACTER,FLOTANTE,COMODIN_ENTERO,COMODIN_FLOTANTE,COMODIN_CARACTER,
+    ID,ID_GLOBAL,SEC_VB,SEC_JV,SEC_PY,SEC_C,CONSTRUCTOR,VOID,ARREGLO,CADENA,CONSTANTE,CLASE,
+    RETURN,PARAM,METODO,REFERENCIA,THIS,stack,heap;
+    
+}
